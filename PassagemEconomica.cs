c268d5824616bcfc0927b40a40ef7950bb0e795f
Adriano Sousa <adrianodesousa1998@gmail.com>
2024-09-16 16:23:29 -0300

    Adicionado a classe derivada para calcular o desconto da classe econômica,

---
+++ PassagemEconomica.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompraDePassagem
+{
+    internal class PassagemEconomica
+    {
+    }
+}
--- obj/Debug/netcoreapp3.1/CompraDePassagem.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/CompraDePassagem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompraDePassagem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128a91c89f6496189b451531d5b8c937151f141c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4270108d493784cab5b0f354d5eb196d76008f57")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompraDePassagem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompraDePassagem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
